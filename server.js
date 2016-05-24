bd36def6ea4957c3ca22ad5736be8b8a7c62a3b2
johntalbotcox@gmail.com <talbotcox@Talbots-MacBook-Pro.local>
2016-05-24 17:44:24 -0600

    added https thing

--- server.js
+++ server.js
@@ -4,15 +4,31 @@ var express 	= require('express'),
 	app 		= express(),
 	port 		= process.env.PORT || 7000,
 	path		= require('path'),
-	apiRoutes 	= require('./api_routes')
+	apiRoutes 	= require('./api_routes'),
+	mongoose	= require('mongoose')
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, './public')))
 
+// Force HTTPS on Heroku
+if (app.get('env') === 'production') {
+ app.use(function (req, res, next) {
+   var protocol = req.get('x-forwarded-proto')
+   protocol == 'https' ? next() : res.redirect('https://' + req.hostname + req.url)
+ })
+}
+app.use(function (req, res, next) {
+ res.setHeader('Access-Control-Allow-Origin', '*')
+ res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+ res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization')
+ next()
+})
+
 app.use('/api', apiRoutes);
 
+mongoose.connect()
 
 app.listen(port, function(err) {
   if(err) console.log('There is an Err in your code:' + err);
